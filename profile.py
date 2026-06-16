822c6acb868e080084b9c97cc988866607bc60c6
Bubblesbear <ancily.liu@gmail.com>
2026-06-16 16:46:21 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,15 @@
+full_name = "Ancily Liu"
+nickname = "Ancily"
+age = 16
+used_python_before = False
+hobbies = ["listening to music", "walking", "watching TV Shows"]
+favorites ={
+    "movie": "Mulan",
+    "food": "Fried Rice",
+    "hobby": "listening to music",
+    "favorite color": "Scarlet",
+    "favorite show": "Gravity Falls"
+}
+print(full_name, age, hobbies)
+skills = ("teamwork", "Flexible")
+languages_spoken = {"English", "Mandarin"}
