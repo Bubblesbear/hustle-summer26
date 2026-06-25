2f70659394f9e1b85149868fa2a501c1c5592773
Bubblesbear <ancily.liu@gmail.com>
2026-06-25 10:08:21 -0700

    Lab 3 Data Structures

---
+++ Data.py
@@ -0,0 +1,6 @@
+faves = ["Tangled", "Mulan", "Soul", "The Wild RObot"]
+faves = {"Tangled" : "Repunzel",
+         "Mulan" : "Mulan", 
+         "Soul" : "22", 
+         "The Wild Robot" : "Ross"}
+print (faves)
